009391f8a392941f0537147db93cd0c3016001a8
afrizal423 <afrizal2499@gmail.com>
2022-06-29 14:41:24 +0700

    Migrate to Laravel Vite
    changes:
    - update version laravel/framework 9.19
    - update version laravel/laravel 9.2.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,11 +14,12 @@
         <!-- Styles -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Nunito:wght@400;600;700&family=Open+Sans&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('vendor/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}">
+        {{-- <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}"> --}}
         <link rel="stylesheet" href="{{ asset('stisla/css/style.css') }}">
         <link rel="stylesheet" href="{{ asset('stisla/css/components.css') }}">
         <link rel="stylesheet" href="{{ asset('vendor/notyf/notyf.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
+        @vite(['resources/js/app.js'])
 
         <link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-shims.min.css" media="all">
         <link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-font-face.min.css" media="all">
@@ -73,7 +74,7 @@
         <script src="{{ asset('stisla/js/scripts.js') }}"></script>
 
         <livewire:scripts />
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        {{-- <script src="{{ mix('js/app.js') }}" defer></script> --}}
 
         @isset($script)
             {{ $script }}
